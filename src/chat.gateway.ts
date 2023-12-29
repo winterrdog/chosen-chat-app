fa2ca1b4cd9b6d212d7846c1cb581934b2e488e9
winterrdog <winterrdog@protonmail.ch>
2023-12-29 21:24:35 +0300

    added web sockets

---
+++ src/chat.gateway.ts
@@ -0,0 +1,19 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway(80)
+export class ChatGateway {
+  @WebSocketServer()
+  private static server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() msg: string): void {
+    // broadcast message to all connected ws clients
+    ChatGateway.server.emit('message', msg);
+  }
+}
