4765d65963fb1c139306f3ab5b6f38e19cba314c
winterrdog <winterrdog@protonmail.ch>
2024-01-03 15:48:58 +0300

    feat: added `chat` history for new members

--- back-end/containerize.sh
+++ back-end/containerize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # pull image from docker hub & run container
-docker container run --detach --name chat-app -p 3000:3000 winterrdog/gsd:chat-v1
+docker container run --detach --name chat-app -p 3000:3000 winterrdog/chat-room-app:1.1.2
--- back-end/src/chat.gateway.ts
+++ back-end/src/chat.gateway.ts
@@ -8,12 +8,19 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+// add small chat history db
+const chatHistory: string[] = [];
+
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayDisconnect, OnGatewayConnection {
   @WebSocketServer()
   private server: Server;
 
   handleConnection(client: Socket): void {
+    // send all current chat history to new client
+    if (chatHistory.length > 0)
+      client.emit('chat-history', { data: chatHistory });
+
     console.log(`[+] Client connected: ${client.id}`);
   }
 
@@ -22,9 +29,14 @@ export class ChatGateway implements OnGatewayDisconnect, OnGatewayConnection {
   }
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() msg: string): void {
+  handleMessage(@MessageBody() wsData: Record<string, any>): void {
+    // update chat history
+    const message: string = wsData.data;
+    chatHistory.push(message);
+
     // broadcast message to all connected ws clients
-    this.server.emit('message', msg);
-    console.log(`[+] Message broadcasted: ${msg}`);
+    this.server.emit('message', wsData);
+
+    console.log(`[+] Message broadcasted: ${message}`);
   }
 }
