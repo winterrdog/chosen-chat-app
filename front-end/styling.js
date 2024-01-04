5222b74408fbdaafa9bf6a90318510b2af28df75
muchubatactics <byooxmckeith@gmail.com>
2024-01-04 22:24:38 +0300

    Messages flowing in the right order

--- front-end/styling.js
+++ front-end/styling.js
@@ -3,9 +3,9 @@ document.querySelector('.submit-div > input').focus();
 let submitButton = document.querySelector('.submit-div > button');
 submitButton.addEventListener('mouseover', () => {
     submitButton.style = 'background-color: rgb(165, 126, 0);';
-    submitButton.childNodes[0].style = 'fill: white;';
+    submitButton.childNodes[1].style = 'fill: white;';
 });
 submitButton.addEventListener('mouseout', () => {
     submitButton.style = 'background-color: white';
-    submitButton.childNodes[0].style = 'fill: rgb(165, 126, 0);';
+    submitButton.childNodes[1].style = 'fill: rgb(165, 126, 0);';
 });
