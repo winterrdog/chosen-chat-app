3d848c3aa796e7291259f18994009a13334fb8ca
winterrdog <winterrdog@protonmail.ch>
2024-01-12 13:27:15 +0300

    allowed adding custom port

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
 (async function () {
   const app = await NestFactory.create(AppModule, {
     cors: true,
     logger: ['error', 'warn', 'log' /* 'debug', 'verbose' */],
   });
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 })();
