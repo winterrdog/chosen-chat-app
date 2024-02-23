50049c1d0e68a644b6206474da45a978c40a8b8e
muchubatactics <byooxmckeith@gmail.com>
2024-02-23 11:53:36 +0300

    style input

--- front-end/styling.js
+++ front-end/styling.js
@@ -2,14 +2,10 @@ document.querySelector('.submit-div > input').focus();
 
 let submitButton = document.querySelector('.submit-div > button');
 submitButton.addEventListener('mouseover', () => {
-    submitButton.style = 'background-color: rgb(165, 126, 0);';
-    submitButton.childNodes[1].style = 'fill: white;';
+    submitButton.style = 'background-color: rgb(255, 246, 195);';
+    // submitButton.childNodes[1].style = 'fill: white;';
 });
 submitButton.addEventListener('mouseout', () => {
     submitButton.style = 'background-color: white';
-    submitButton.childNodes[1].style = 'fill: rgb(165, 126, 0);';
+    // submitButton.childNodes[1].style = 'fill: rgb(255, 246, 195);';
 });
-document.getElementById('message').addEventListener('input', () => {
-    console.log('here');
-    window.scrollTo(0, document.body.scrollHeight);
-})
